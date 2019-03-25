5016114440e2df2a976ecc9038f4fd8fd5b03a0e
Mario Esteban Viapiano <marioeviapiano@gmail.com>
2019-03-25 14:57:18 +0100

    Add elasticsearch

--- Gemfile
+++ Gemfile
@@ -4,5 +4,6 @@ source 'https://rubygems.org'
 
 gem 'dry-auto_inject'
 gem 'dry-container'
+gem 'elasticsearch'
 gem 'roda'
 gem 'slim'
--- adapters/gateways/elastic_search/search.rb
+++ adapters/gateways/elastic_search/search.rb
@@ -1,13 +1,36 @@
+require 'elasticsearch'
+
 module Adapters
   module Gateways
     module ElasticSearch
       # Implementation of a search function usign ElasticSearch
       class Search
-        def call(_params)
-          [
-            { id: 1, file_name: 'name1', file_path: 'path1' },
-            { id: 2, file_name: 'name2', file_path: 'path2' }
-          ]
+        def initialize
+          @client = Elasticsearch::Client.new url: ENV['ELASTICSEARCH_URL']
+        end
+
+        def call(params)
+          raw_results_for(params[:q]).map do |result|
+            {
+              file_name: result['_source']['file_name'],
+              file_path: result['_source']['file_path'],
+              tags: result['_source']['tags'],
+              id: result['_id']
+            }
+          end
+        end
+
+        private
+
+        attr_reader :client
+
+        def raw_results_for(keyword)
+          client.search(index: 'mavi',
+                        body: {
+                          query: { match: { file_name: keyword } },
+                          from: 0,
+                          size: 10
+                        })['hits']['hits']
         end
       end
     end
